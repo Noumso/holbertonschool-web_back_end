b903d927efe5cbe57f811c6d95a046d59a188315
Noumso <noumane.82300@gmail.com>
2025-03-27 14:48:25 +0000

    A Building

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,29 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = sqft;
+
+    if (this.constructor !== Building
+      && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(valeur) {
+    if (typeof valeur !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = valeur;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
