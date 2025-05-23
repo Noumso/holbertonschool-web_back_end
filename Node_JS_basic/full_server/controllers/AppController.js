d090313abb346acc80971634d020d55a54598d00
Noumso <159029742+Noumso@users.noreply.github.com>
2025-05-23 15:51:05 +0000

    Fix formatting and add missing newlines in multiple files

--- Node_JS_basic/full_server/controllers/AppController.js
+++ Node_JS_basic/full_server/controllers/AppController.js
@@ -1,5 +1,5 @@
 export default class AppController {
-    static getHomepage(request, response) {
-      response.send(200, 'Hello Holberton School!');
-    }
+  static getHomepage(_request, response) {
+    response.status(200).send('Hello Holberton School!');
   }
+}
